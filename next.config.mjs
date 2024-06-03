b8545765da11e611906fe204a0b23b0b0f85575a
Enzo-21 <enzo21barrera@gmail.com>
2024-06-03 16:25:25 -0300

    Added maskable icon prop

--- next.config.mjs
+++ next.config.mjs
@@ -9,8 +9,8 @@ const pwaConfig = withPWA({
         dest: "public",
         register: true,
         skipWaiting: true,
-        // Uncomment the following line to test in development environment. Pay attention to the console messages about GenerateSW. If you want to update the precache manifest, feel free to delete the automatically generated files when needed to generate new ones.
-        disable: process.env.NODE_ENV === 'development'
+        // Comment the following line to test in development environment. Pay attention to the console messages about GenerateSW. If you want to update the precache manifest, feel free to delete the automatically generated files when needed to generate new ones.
+        //disable: process.env.NODE_ENV === 'development'
 });
 
 export default pwaConfig(nextConfig);
